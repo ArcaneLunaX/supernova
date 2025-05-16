c004c8dc3b34947dca3f59ee1574dfa7c3741ae8
Veis <arcanelunax@gmail.com>
2025-05-16 12:31:44 +0100

    Exciting! got chisel to work with block below it too, its omega pog.

--- src/main/java/com/veis/supernova/common/item/custom/ChiselItem.java
+++ src/main/java/com/veis/supernova/common/item/custom/ChiselItem.java
@@ -12,6 +12,7 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.apache.logging.log4j.core.jmx.Server;
 
 import java.util.Map;
 
@@ -20,7 +21,8 @@ public class ChiselItem extends Item {
             Map.of(
                     Blocks.STONE, Blocks.STONE_BRICKS,
                     Blocks.SANDSTONE, Blocks.SMOOTH_SANDSTONE,
-                    ModBlocks.SUPERNOVA_CATALYST.get(), ModBlocks.SUPERNOVA_BLOCK.get()
+                    ModBlocks.SUPERNOVA_CATALYST.get(), ModBlocks.SUPERNOVA_BLOCK.get(),
+                    ModBlocks.SUPERNOVA_SOURCE.get(), Blocks.STONE
             );
     public ChiselItem(Properties properties) {
         super(properties);
@@ -30,12 +32,19 @@ public class ChiselItem extends Item {
     public InteractionResult useOn(UseOnContext context) {
         Level level = context.getLevel();
         Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock();
+        Block belowBlock = level.getBlockState(context.getClickedPos().below()).getBlock();
         if(CHISEL_MAP.containsKey(clickedBlock)) {
             if(!level.isClientSide()) {
                 level.setBlockAndUpdate(context.getClickedPos(), CHISEL_MAP.get(clickedBlock).defaultBlockState());
 
                 context.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), context.getPlayer(),
                         item -> context.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+                if(belowBlock == ModBlocks.SUPERNOVA_SOURCE.get()) {
+                    context.getItemInHand().hurtAndBreak(10, ((ServerLevel) level), context.getPlayer(),
+                            item -> context.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+                    level.setBlockAndUpdate(context.getClickedPos().below(), CHISEL_MAP.get(belowBlock).defaultBlockState());
+                }
+
 
                 level.playSound(null, context.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
             }
